bc3ba60dbec8ede6e546da1a1b328655b00c1d85
Ye Paing <ypaing@ypaing-c02fk55zmd6r.fios-router.home>
2022-03-02 12:14:00 -0500

    init commit

---
+++ wordle_solver.js
@@ -0,0 +1,9 @@
+const enterKey = (k) => {
+	window.dispatchEvent(new KeyboardEvent('keydown', {
+	  'key': k
+	}));
+}
+
+JSON.parse(localStorage['nyt-wordle-state']).solution.split('').forEach(c => enterKey(c));
+
+enterKey("Enter");
