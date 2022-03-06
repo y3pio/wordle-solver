b2972f740c1ff8b56bab7cb6f930bdd5cfd4c46f
Ye Paing <hello.paingy@gmail.com>
2022-03-06 03:10:45 -0500

    Update wordle_solver.js
    tab formatting

--- wordle_solver.js
+++ wordle_solver.js
@@ -1,8 +1,8 @@
 const enterKey = (k) => {
-	window.dispatchEvent(new KeyboardEvent('keydown', {
-	  'key': k
-	}));
-}
+  window.dispatchEvent(new KeyboardEvent(
+		'keydown', {'key': k}
+	));
+};
 
 JSON.parse(localStorage['nyt-wordle-state']).solution.split('').forEach(c => enterKey(c));
 
